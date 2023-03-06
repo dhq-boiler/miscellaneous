d422cd3d8bdaabe32a263f0c9671dcb5b278eb27
dhq_boiler <dhq_boiler@live.jp>
2023-03-07 00:29:14 +0900

    initial commit

---
+++ ReconnectVPN/ReconnectVPN/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using ReconnectVPN.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReconnectVPN
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var window = Container.Resolve<MainWindow>();
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
