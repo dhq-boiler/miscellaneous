54fca1068a86ddca0a5754089751fedf7fdf34eb
dhq_boiler <dhq_boiler@live.jp>
2023-03-07 20:50:35 +0900

    add cloned ReconnectVPN3

---
+++ ReconnectVPN3/ReconnectVPN/Helpers/PasswordManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+using Windows.Security.Credentials.UI;
+
+namespace ReconnectVPN.Helpers
+{
+    public static class PasswordManager
+    {
+        public static void SetPassword(string vpnName, string password, string userName)
+        {
+            PasswordVault myVault = new PasswordVault();
+            myVault.Add(new PasswordCredential(vpnName, userName, password));
+        }
+
+        public static void RemovePassword(string vpnName, string userName)
+        {
+            PasswordVault myVault = new PasswordVault();
+
+            var password = myVault.Retrieve(vpnName, userName);
+            if (password != null)
+                myVault.Remove(password);
+        }
+
+        public static async Task<string> SignInAsync(string vpnName, string userName)
+        {
+            var result = await UserConsentVerifierInterop.RequestVerificationForWindowAsync(Process.GetCurrentProcess().MainWindowHandle, "このVPNにアクセスするには資格情報が必要です。");
+            if (result != UserConsentVerificationResult.Verified)
+                return null;
+
+            try
+            {
+                var vault = new PasswordVault();
+                var credentials = vault.Retrieve(vpnName, userName);
+                return credentials?.Password;
+            }
+            catch (COMException)
+            {
+                return null;
+            }
+        }
+    }
+}
