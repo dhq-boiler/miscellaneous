4e7364f989a4bcfe868bda2c744bfcde02342b99
Takashi Homma <takashi.honma@luxiar.com>
2023-07-28 11:44:28 +0900

    yield return / yield break 説明用のコンソールプログラムを追加

---
+++ yield/yield/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+IEnumerable<int> MethodForYield()
+{
+    yield return 1;
+    yield return 2;
+    yield return 3;
+    yield break;
+    yield return 4;
+    yield return 5;
+    yield return 6;
+}
+
+foreach (var intVal in MethodForYield())
+{
+    Console.WriteLine(intVal);
+}
+
